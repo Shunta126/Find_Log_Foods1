266b279e2feffa1fd1db84e89b20630a6d5aa225
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-10-09 16:45:02 +0000

    cretate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,32 @@
 Rails.application.routes.draw do
-  # 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
+ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
-# 管理者用
-# URL /admin/sign_in ...
+scope module: :public do
+  root to: 'homes#top'
+  get 'about' => 'homes#about'
+  get 'customers', to: 'customers#index'
+  get 'customers/:id/my_page', to: 'customers#show', as: 'customer'
+  get 'customers/:id/information/edit', to: 'customers#edit', as: 'edit_customer'
+  patch 'customers/:id/information', to: 'customers#update', as: 'update_customer'
+  get 'customers/:id/confirm', to: 'customers#confirm', as: 'confirm_customer'
+  patch 'customers/:id/withdrawal', to: 'customers#withdrawal', as: 'withdrawal_customer'
+  resources :restaurants do
+    resources :comments, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
+  end
+end
+
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+namespace :admin do
+  root to: 'homes#top'
+  resources :genres, only: [:index, :create, :edit, :update]
+  resources :customers, only: [:show, :edit, :update]
+  resources :restaurants, only: [:index, :show, :edit, :destroy]
+ end
 end
