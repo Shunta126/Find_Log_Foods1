c9b901bd22791460b4c2655dc832cebf3302b584
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-10-09 18:23:27 +0000

    add customer log_in_page

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,35 @@
 class Public::CustomersController < ApplicationController
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+     @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer =Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+
+  def confirm
+  end
+
+  def withdrawal
+  end
+
 end
+
+ private
+   def customer_params
+     params.require(:customer).permit(:name, :body, :email, :age, :encrypted_password, :profile_image)
+   end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,16 @@
 class Customer < ApplicationRecord
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :profile_image, presence: true
+  validates :body, length:{maximum:200}
+  validates :email, presence: true, uniqueness: true
+
 end
