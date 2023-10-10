3780debc0bd3d079a9e2c251707c36326f114171
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-10-10 12:32:20 +0000

    create restaurant_new_page

--- app/controllers/public/restaurants_controller.rb
+++ app/controllers/public/restaurants_controller.rb
@@ -1,2 +1,40 @@
 class Public::RestaurantsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @restaurant = Restaurant.new
+    @genres = Genre.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.customer_id = current_customer.id
+    if @restaurant.save
+      flash[:notice] = "投稿しました！"
+      redirect_to restaurants_path
+    else
+      @genres = Genre.all
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
+
+private
+   def restaurant_params
+     params.require(:restaurant).permit(:restaurant_name, :genre_id, :food_name, :price, :body, :place, :image)
+   end
