9e936402abb459e491f797d39ed87835b422d196
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-10-12 14:15:11 +0000

    create like_Auto

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,17 +1,15 @@
 class Public::LikesController < ApplicationController
 
   def create
-    restaurant = Restaurant.find(params[:restaurant_id])
-    like = current_customer.likes.new(restaurant_id: restaurant.id)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    like = current_customer.likes.new(restaurant_id: @restaurant.id)
     like.save
-    redirect_to restaurant_path(restaurant)
   end
 
   def  destroy
-    restaurant = Restaurant.find(params[:restaurant_id])
-    like = current_customer.likes.find_by(restaurant_id: restaurant.id)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    like = current_customer.likes.find_by(restaurant_id: @restaurant.id)
     like.destroy
-    redirect_to restaurant_path(restaurant)
   end
 
 end
