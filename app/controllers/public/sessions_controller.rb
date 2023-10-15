6d97b62eb58ca72ca0643a884f0c1ceff41085e1
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-10-15 15:00:38 +0000

    is_delete

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+   before_action :customer_state, only: [:create]
 
   def after_sign_up_path_for(resource)
     customers_path
@@ -11,6 +11,14 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_deleted
+      flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+      redirect_to new_customer_registration_path
+    end
+  end
   # GET /resource/sign_in
   # def new
   #   super
