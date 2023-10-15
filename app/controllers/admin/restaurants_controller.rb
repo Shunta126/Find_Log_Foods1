4c391a8f7c4d52c785296164a2ebc16102018b53
EC2 Default User <141287848+Shunta126@users.noreply.github.com>
2023-10-15 17:02:31 +0000

    create admin_restaurant_page

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,2 +1,46 @@
 class Admin::RestaurantsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @restaurants = @genre.restaurants.all
+    else
+      @restaurants = Restaurant.all
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "変更しました！"
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      @genres = Genre.all
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to admin_restaurants_path
+  end
+
 end
+
+private
+   def restaurant_params
+     params.require(:restaurant).permit(:restaurant_name, :genre_id, :food_name, :price, :body, :place, :image)
+   end
--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,6 @@ namespace :admin do
   root to: 'homes#top'
   resources :genres, only: [:index, :create, :edit, :update]
   resources :customers, only: [:show, :edit, :update]
-  resources :restaurants, only: [:index, :show, :edit, :destroy]
+  resources :restaurants, only: [:index, :show, :edit, :destroy, :update]
  end
 end
